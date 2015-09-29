d5c6d5148ca6d9c955c71f9bb0e49f3bbe264e70
Tony DiNitto <think41c@gmail.com>
2015-09-28 23:57:02 -0500

    Logic beginning to establish.

--- sumofnth.rb
+++ sumofnth.rb
@@ -1,7 +1,12 @@
 class SumOfNth
 
+  def denominator 
+    # count from 1 and add 3 as many times as requested from user.
+  end
+
 	def series_generator
     # Generate this - 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16...
+    # numerator is always 1. 
   end 
 
   def add_series(x) 
