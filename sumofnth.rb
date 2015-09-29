db9673c771dd55333d74d2a81b1fef27a328ff58
Tony DiNitto <think41c@gmail.com>
2015-09-28 23:53:15 -0500

    Two basic methods established to add the series.

--- sumofnth.rb
+++ sumofnth.rb
@@ -1,3 +1,11 @@
 class SumOfNth
 
+	def series_generator
+    # Generate this - 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16...
+  end 
+
+  def add_series(x) 
+    # Add x amount of values in the series. 
+  end
+
 end
