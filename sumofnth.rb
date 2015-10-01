ec4826d0b0e6e368280119f25a1c2264112ebf1a
Tony DiNitto <think41c@gmail.com>
2015-09-30 21:35:02 -0500

    Removed pointless first_num assignement.'

--- sumofnth.rb
+++ sumofnth.rb
@@ -1,9 +1,9 @@
 class SumOfNth
 
   def denominator
-    denomator_numbers = [] 
+    denomator_numbers = []
     beginning = 1
-    @user_number.times do 
+    @user_number.times do
       denomator_numbers << beginning
       puts "Number adding to the array is -> #{beginning}"
       beginning += 3
@@ -14,18 +14,16 @@ class SumOfNth
 	def series_generator(dem)
     p "The sequence"
     start = 0
-    @user_number.times do 
-      p first_num = 1.to_f / dem[start].to_f
+    @user_number.times do
+      p 1.to_f / dem[start].to_f
       start += 1
     end
+  end
 
-
-  end 
-
-  def add_series(x) 
-    @user_number = x 
+  def add_series(x)
+    @user_number = x
     puts "You want to add #{x} of the numbers in the series together."
-    a = denominator 
+    a = denominator
     p a[1]
     p 1/4.to_f
     answer = 1.to_f / a[1].to_f
